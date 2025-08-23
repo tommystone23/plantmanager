4a37cc182397d19bd423a2a949eeee1016054edb
Thomas Stone <tommy.stone23@outlook.com>
2025-08-22 20:52:25 -0400

    frontend: add image upload through new api endpoint, and display returned image data.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,55 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 function App() {
-  const [message, setMessage] = useState("Loading...");
+  const [file, setFile] = useState(null);
+  const [image, setImage] = useState("")
+  const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((err) => {
-        console.error("Error fetching:", err);
-        setMessage("Failed to load message.");
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    if (!file) {
+      setMessage("Please choose a file first.");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("image", file);
+
+    try {
+      const res = await fetch("/api/upload/image", {
+        method: "POST",
+        body: formData,
       });
-  }, []);
+
+      if(res.ok)
+        setMessage("File Uploaded Successfully")
+
+      const data = await res.json()
+      setImage(data.image)
+
+    } catch (err) {
+      console.error(err);
+      setMessage("Error uploading.");
+    }
+  };
 
   return (
-    <div style={{ fontFamily: "Arial, sans-serif", padding: "2rem" }}>
-      <h1>Plant Manager Landing Page</h1>
+    <div style={{ padding: "2rem" }}>
+      <h1>Plant Manager</h1>
+      <h2>Upload a Photo</h2>
+      <input type="file" accept="image/*" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
       <p>{message}</p>
+
+      {image && (
+        <div style={{ marginTop: "1rem" }}>
+          <h3>Preview:</h3>
+          <img src={image} alt="Preview" style={{ maxWidth: "300px" }} />
+        </div>
+      )}
     </div>
   );
 }
